b47240de4620a57a6020c16098d8ca0870950680
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2025-04-30 21:45:35 +0300

    Начало формы

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,97 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>
-    <el-button>test</el-button>
+    <el-row>
+      <!-- <el-alert type="info" description="Для указания нескольких" show-icon /> -->
+      <el-form :inline="false" :model="form" class="form" label-position="top">
+        <el-row :gutter="10" class="form-item">
+          <el-col :span="4">
+            <el-form-item label="Метки">
+              <el-input v-model="form.marks" placeholder="Метки" clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="5">
+            <el-form-item label="Тип записи">
+              <el-select v-model="form.recordType" placeholder="Тип записи" clearable>
+                <el-option label="Локальная" value="local" />
+                <el-option label="LDAP" value="ldap" />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="Логин">
+              <el-input v-model="form.login" placeholder="Значение"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="Пароль">
+              <el-input v-model="form.password" show-password></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="3">
+            <div class="delete-btn-wrapper">
+              <el-form-item>
+                <el-button plain :icon="Delete" type="primary" @click="onSubmit"></el-button>
+              </el-form-item>
+            </div>
+          </el-col>
+        </el-row>
+      </el-form>
+    </el-row>
   </main>
 </template>
 
-<style scoped></style>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { Delete } from '@element-plus/icons-vue'
+
+interface Form {
+  marks: string
+  recordType: string
+  login: string
+  password: string
+}
+
+const form = reactive<Form>({
+  marks: '',
+  recordType: '',
+  login: '',
+  password: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+<style>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100dvh;
+  width: 100dvw;
+}
+
+.form {
+  width: 100%;
+  /* height: 70%; */
+  background-color: rgba(0, 0, 0, 0.136);
+  padding: 1.5rem;
+}
+
+.delete-btn-wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+/* .form .el-input {
+  --el-input-width: 100px;
+}
+
+.form .el-select {
+  --el-select-width: 100px;
+} */
+</style>
